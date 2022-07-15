191dddc9ea1b2db4cf8e5d0f5906641f30b552bc
Hwangyunji <105402299+Hwangyunji@users.noreply.github.com>
2022-07-15 16:11:37 +0900

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,12 +1,25 @@
-$(function () {
-
-    // 1. 열기: #toggle-btn 클릭시 #gnb on
-    $('#icon-menu').click(function () {
+$(function(){
+    // ---------gnb-----------------
+      // 1. 열기: #toggle-btn 클릭시 #gnb on
+      $('#toggle-btn').click(function(){
         $('#gnb').addClass('on');
-    });
-    // 2. 닫기: #btn-close 클릭시 #gnb 닫음
-    $('#btn-close').click(function () {
+      });
+      // 2. 닫기: #btn-close 클릭시 #gnb 닫음
+      $('#btn-close').click(function(){
         $('#gnb').removeClass('on');
-    });
+      });
 
-}); // $
+
+      const swiper = new Swiper('#main .swiper', {
+
+        
+        navigation: {
+          // class명 지정
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      
+      });
+      
+});
+
