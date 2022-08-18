4b3e05b4f59033091551437b764cd5a7769287a4
hanabeeeee <105402245+hanabeeeee@users.noreply.github.com>
2022-08-18 11:27:39 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,16 +1,35 @@
 # seocho-animal-mobile
 
-https://hwangyunji.github.io/seocho-animal-mobile/
+
+https://hanabeeeee.github.io/Seocho-Animals-center/
 
 # 기존 사이트
+
+
 https://seocho.go.kr/site/animal/main.do
 
 # 제작 목표
+
+
 - 카테고리 간소화(불필요한 카테고리 메뉴 삭제/수정)를 포함한 메뉴 개선
 - 모바일 웹에 최적화 된 페이지 제작으로 가독성 개선
 
 # 구현 기능
+
+
 - 메인 페이지 우측 하단에 카카오톡 채널 문의 버튼 고정
 - 메인 페이지 보호 동물 이미지 자동 재생과 화살표 아이콘으로 이전/다음 이미지 확인 가능
 - Toggle category menu Hover 기능 추가
 - 입양 공고 상세페이지 하단 입양 상담하기 버튼 클릭 시 전화번호 연결(모바일 디바이스만 가능)
+- GNB Hover 시 서브 메뉴 나오게 구현
+
+# 나의 기여
+
+
+- Rule, Notice 페이지 구현
+- Rule의 아이콘 직접 제작
+- Detail 페이지 오류 전면 수정
+- Main의 fixed 버튼 구현
+- Header, GNB 구현
+- Footer 오류 전면 수정
+
--- js/app.js
+++ js/app.js
@@ -1,30 +1,29 @@
 $(function(){
-  // ---------gnb-----------------
-    // 1. 열기: #toggle-btn 클릭시 #gnb on
-    $('#toggle-btn').click(function(){
-      $('#gnb').addClass('on');
-    });
-    // 2. 닫기: #btn-close 클릭시 #gnb 닫음
-    $('#btn-close').click(function(){
-      $('#gnb').removeClass('on');
-    });
-
-
-    const swiper = new Swiper('#main .swiper', {
+    // ---------gnb-----------------
+      // 1. 열기: #toggle-btn 클릭시 #gnb on
+      $('#toggle-btn').click(function(){
+        $('#gnb').toggleClass('on');
+      });
+      // 2. 닫기: #btn-close 클릭시 #gnb 닫음
+      $('#btn-close').click(function(){
+        $('#gnb').removeClass('on');
+      });
   
-      autoplay: {
-        delay: 5000,
-      },
-      direction: 'horizontal',  // 효과
-      loop: true,
+  
+      const swiper = new Swiper('#main .swiper', {
+  
+        autoplay: {
+          delay: 5000,
+        },
+        direction: 'horizontal',  // 효과
+        loop: true,
+        
+        navigation: {
+          // class명 지정
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
       
-      navigation: {
-        // class명 지정
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    
-    });
-    
-});
-
+      });
+      
+  });
